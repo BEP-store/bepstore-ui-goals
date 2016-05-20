137bbe9442cbb6877b825d92b5118ad92c3fb3d5
Bart Heemskerk <de.uil.met.een@gmail.com>
2016-05-20 12:43:18 +0200

    placed helper in component

--- addon/components/goal/core-team.js
+++ addon/components/goal/core-team.js
@@ -2,5 +2,36 @@ import layout from 'bepstore-goals/templates/components/goal/core-team';
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  layout
+  layout,
+  _getPO: Ember.computed('team', function() {
+    let po = this.get('team').po;
+    if(po){
+      return po;
+    }
+      return "?";
+    }),
+
+  _getHD: Ember.computed('team', function() {
+    let hd = this.get('team').hd;
+    if(hd){
+      return hd;
+    }
+      return "?";
+    }),
+
+  _getHFE: Ember.computed('team', function() {
+    let hfe = this.get('team').hfe;
+    if(hfe){
+      return hfe;
+    }
+      return "?";
+    }),
+
+  _getHBE: Ember.computed('team', function() {
+    let hbe = this.get('team').hbe;
+    if(hbe){
+      return hbe;
+    }
+      return "?";
+    })
 });
