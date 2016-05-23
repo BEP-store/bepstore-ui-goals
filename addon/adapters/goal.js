46f2d663e1a4da24cb049af8b0e2461886afa3cd
Wouter Kooyman van Guldener <wouter@wick-it.nl>
2016-05-23 14:25:07 +0200

    Delete goalAdapter because it is not used anymore.

--- addon/adapters/goal.js
+++
@@ -1,6 +0,0 @@
-// FIXME: This works by magic now, since it just assumes the engine adapter exists.
-import EngineAdapter from './engine';
-
-export default EngineAdapter.extend({
-  engine: 'goals'
-});
