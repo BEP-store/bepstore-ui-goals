302d3530a50a5a3240d9ab60bccbc2ce0b2b493e
Bart Heemskerk <de.uil.met.een@gmail.com>
2016-05-23 17:37:17 +0200

    added link to create new goal, backend auth needs fix

--- addon/components/new-view.js
+++ addon/components/new-view.js
@@ -7,29 +7,14 @@ const { inject: { service } } = Ember;
 export default ActivitiesBaseNewView.extend({
   layout,
   account: service(),
+  groups: null,
+  parents: null,
+  contributors: null,
 
-  init() {
-    this._super(...arguments);
-  },
+  init(){
+   this._super(...arguments);
 
-  actions: {
-    save(){
-      /* jshint ignore:start */
-      let save = this._super.bind(this, ...arguments);
-
-      let attrs = {
-        title: this.get('model.title'),
-        description: this.get('model.description')
-      };
-
-      let model = this.get('store').createRecord('goal', {
-        ...attrs
-      }).save();
-
-      this.get('model').deleteRecord();
-      this.set('model', model);
-      save();
-      /* jshint ignore:end */
-    }
-  }
+   this.set('groups', []);
+   this.set('parents', []);
+ }
 });
