a6c5d855fdc22ce050cb642344c3bc8e950cbee1
Bart Heemskerk <de.uil.met.een@gmail.com>
2016-05-23 12:11:57 +0200

    implemented new-view draft

--- addon/components/new-view.js
+++ addon/components/new-view.js
@@ -2,5 +2,30 @@ import ActivitiesBaseNewView from 'feedbackfruits-activities/components/new-view
 import layout from '../templates/components/new-view';
 
 export default ActivitiesBaseNewView.extend({
-  layout
+  layout,
+
+  init() {
+    this._super(...arguments);
+  },
+
+  actions: {
+    save(){
+      /* jshint ignore:start */
+      let save = this._super.bind(this, ...arguments);
+
+      let attrs = {
+        title: this.get('model.title'),
+        description: this.get('model.description')
+      };
+
+      let model = this.get('store').createRecord('goal', {
+        ...attrs
+      }).save();
+
+      this.get('model').deleteRecord();
+      this.set('model', model);
+      save();
+      /* jshint ignore:end */
+    }
+  }
 });
