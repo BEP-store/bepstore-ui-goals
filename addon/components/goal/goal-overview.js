e132cd0318229a9f6129cd70dfe99bc01daf5fa3
Bart Heemskerk <de.uil.met.een@gmail.com>
2016-05-25 20:50:03 +0200

    fixed modal

--- addon/components/goal/goal-overview.js
+++ addon/components/goal/goal-overview.js
@@ -1,11 +1,24 @@
 import layout from 'bepstore-goals/templates/components/goal/goal-overview';
 import Ember from 'ember';
 
+const { inject: { service } } = Ember;
+
 export default Ember.Component.extend({
   layout,
+  router: service(),
+
   actions: {
     showDescr() {
       this.toggleProperty('showAll');
+    },
+    open() {
+      if(this.get('activitiesShowRoute')){
+          this.get('router').transitionTo(this.get(this.get('activitiesShowRoute')), this.get('model.id'));
+      }
+      else
+      {
+          this.get('router').transitionTo(`${this.get('route')}.goal.show`, this.get('model.id'));
+      }
     }
   }
 });
