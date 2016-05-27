356479e6ee4b1f60cc8b145f93d1c1d54d5f74c7
Bart Heemskerk <de.uil.met.een@gmail.com>
2016-05-27 14:59:30 +0200

    refruitment via add-button

--- addon/components/goal/core-team.js
+++ addon/components/goal/core-team.js
@@ -22,19 +22,19 @@ export default Ember.Component.extend({
       });
     this.get('team.head_design').then((hd) => {
         if(hd){
-          this.set('_hd', po.get('name'));
+          this.set('_hd', hd.get('name'));
           this.toggleProperty('hdFlag');
         }
       });
-    this.get('team.head_frontend').then((hbe) => {
-        if(hbe){
-          this.set('_hbe', po.get('name'));
+    this.get('team.head_frontend').then((hfe) => {
+        if(hfe){
+          this.set('_hfe', hfe.get('name'));
           this.toggleProperty('hfeFlag');
         }
       });
     this.get('team.head_backend').then((hbe) => {
         if(hbe){
-          this.set('_hbe', po.get('name'));
+          this.set('_hbe', hbe.get('name'));
           this.toggleProperty('hbeFlag');
         }
       });
