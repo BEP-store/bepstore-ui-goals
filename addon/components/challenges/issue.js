0c92c9609ccaaa3c202899bc2a2be48383f8c368
Bart Heemskerk <de.uil.met.een@gmail.com>
2016-06-08 15:14:43 +0200

    processed feedback

--- addon/components/challenges/issue.js
+++ addon/components/challenges/issue.js
@@ -2,26 +2,5 @@ import layout from 'bepstore-goals/templates/components/challenges/issue';
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  layout,
-  getTitle: Ember.computed('model', function() {
-    let title = this.get('model.title');
-    if(title.length > 50){
-      return title.substr(0,42) + '...';
-    }
-    else
-    {
-      return title;
-    }
-  }),
-
-  idSize: Ember.computed('model', function() {
-    let id = this.get('model.repo.id');
-    if(id.length > 28){
-      return 'small '.htmlSafe();
-    }
-    else
-    {
-      return '';
-    }
-  })
+  layout
 });
