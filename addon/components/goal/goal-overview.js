ca41abf3c440adc8a9eabcdfcd383704b7e979ea
Wouter Kooyman van Guldener <wouter@wick-it.nl>
2016-06-17 02:43:57 +0200

    Goals are now reactive.

--- addon/components/goal/goal-overview.js
+++ addon/components/goal/goal-overview.js
@@ -1,7 +1,7 @@
 import layout from 'bepstore-goals/templates/components/goal/goal-overview';
 import Ember from 'ember';
 
-const { inject: { service } } = Ember;
+const { computed, inject: { service } } = Ember;
 
 export default Ember.Component.extend({
   layout,
@@ -14,11 +14,19 @@ export default Ember.Component.extend({
     open() {
       if(this.get('activitiesShowRoute')){
           this.get('router').transitionTo(this.get(this.get('activitiesShowRoute')), this.get('model.id'));
+
       }
       else
       {
           this.get('router').transitionTo(`${this.get('route')}.goal.show`, this.get('model.id'));
       }
     }
-  }
+  },
+  sortedReviews: computed.sort('model', 'sortDefinition'),
+  sortBy: 'contributors.length', // default sort by date
+  reverseSort: true, // default sort in ascending order
+  sortDefinition: computed('sortBy', 'reverseSort', function() {
+    let sortOrder = this.get('reverseSort') ? 'desc' : 'asc';
+    return [ `${this.get('sortBy')}:${sortOrder}` ];
+  })
 });
