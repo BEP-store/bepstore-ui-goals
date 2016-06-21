6a1942520fd1d71d172da21cd372a05f276e93f2
Bart Heemskerk <de.uil.met.een@gmail.com>
2016-06-21 12:46:53 +0200

    add link to product owner

--- addon/components/goal/goal-menu/core.js
+++ addon/components/goal/goal-menu/core.js
@@ -2,11 +2,5 @@ import layout from 'bepstore-goals/templates/components/goal/goal-menu/core';
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  layout,
-
-  actions: {
-    toUser(id){
-      this.transitionToRoute('users.show',id);
-    }
-  }
+  layout
 });
